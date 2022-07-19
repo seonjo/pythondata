09483c33aa527d298595aa56ad3a498685ed37be
seonjo <s.seonjo@gmail.com>
2022-07-19 12:21:07 +0900

    2022/07/19

--- python_basic/10_flask/app.py
+++ python_basic/10_flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,redirect,render_template
-import sqlite3
-import os
+import os,pickle,sqlite3
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 path = os.path.dirname(__file__)
@@ -38,6 +39,23 @@ def customerlist():
     data = cur.fetchall()
     print(data)
     return render_template('customerlist.html',data=data)
+
+@app.route('/mnist', methods=['GET','POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnist_form.html')
+    else:
+        f = request.files['file']
+        mnistpath = path+'/static/upload/'+f.filename
+        f.save(mnistpath)
+        img = Image.open(mnistpath).convert('L')
+        img = np.resize(img,(1,784))
+        img = 255-img
+        f1 = open(path+'/model','rb')
+        model = pickle.load(f1)
+        data = model.predict(img)
+        print(data)
+        return render_template('mnist_result.html',data = data[0],path=f.filename)
 
 if __name__ == '__main__':
     app.run(debug=True,port=80)
